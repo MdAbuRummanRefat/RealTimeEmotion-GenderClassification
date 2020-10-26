205931cf482b7d3323482cce85942dbb8e31855e
MdAbuRummanRefat <refatiuice@gmail.com>
2020-10-26 14:01:41 +0600

    Create datasets.py

---
+++ utils/datasets.py
@@ -0,0 +1,38 @@
+from scipy.io import loadmat
+import pandas as pd
+import numpy as np
+from random import shuffle
+import os
+import cv2
+
+
+class DataManager(object):
+    """Class for loading fer2013 emotion classification dataset or
+        imdb gender classification dataset."""
+    def __init__(self, dataset_name='imdb', image_size=(48, 48)):
+
+        self.dataset_name = dataset_name
+        self.image_size = image_size
+        
+  def get_labels(dataset_name):
+      if dataset_name == 'fer2013':
+         return {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
+                 4: 'sad', 5: 'surprise', 6: 'neutral'}
+     elif dataset_name == 'imdb':
+         return {0: 'woman', 1: 'man'}
+      elif dataset_name == 'KDEF':
+         return {0: 'AN', 1: 'DI', 2: 'AF', 3: 'HA', 4: 'SA', 5: 'SU', 6: 'NE'}
+     else:
+         raise Exception('Invalid dataset name')
+
+
+  def get_class_to_arg(dataset_name='fer2013'):
+     if dataset_name == 'fer2013':
+          return {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'sad': 4,
+                  'surprise': 5, 'neutral': 6}
+     elif dataset_name == 'imdb':
+         return {'woman': 0, 'man': 1}
+     elif dataset_name == 'KDEF':
+          return {'AN': 0, 'DI': 1, 'AF': 2, 'HA': 3, 'SA': 4, 'SU': 5, 'NE': 6}
+     else:
+         raise Exception('Invalid dataset name')
